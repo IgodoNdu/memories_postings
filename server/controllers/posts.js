499b1c9eee643cea6900d3259f3191633bf86c02
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-14 13:22:10 +0100

    Model Import for Controller actions
    Imported the model to perform actual logic/control of the routers requests/actions (getPosts, createPost)

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,19 @@
 //Here we create the handlers for our routes for cleaner/efficient code. Extract the logic of the routes, and bring in here
+//Import the Model (access to the db model) required to do this (getPosts, createPosts)
+import PostMessage from '../models/postMessage.js'
 
 //declare a function for the posts logic, and export it for use in the required router
-export const getPosts = (req, res) => {
-    res.send('DOES THIS WORK!');
+export const getPosts = async (req, res) => {
+    //res.send('DOES THIS WORK!');
+    try {
+        //retrieve all posts from the DB (takes time, hence asynchronous)
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);//checks purpose only
+        //if all works fine
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
 //function for the post creation route logic
