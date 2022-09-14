a615ae644731861b8654c2209d57e7ba4b48b406
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-14 12:22:58 +0100

    Use the new route via middleware
    Import the created route for use, connect the imported route to the app via middleware use

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+//Import the posts router already created
+import postRoutes from './routes/posts.js';
 
 //as it's traditional practice with express, initialize the app
 const app = express();
+
+//use express middleware to connect the routes to our application
+//In the first param:set the starting path(prefix) for all the routes inside posts.js, 2nd param: set the route
+app.use('/posts', postRoutes)
 
 //setting up bodyParser to send our requests(post) as needed
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -12,8 +18,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //for cross origin request
 app.use(cors());
 
+
 //database: https://www.mongodb.com/cloud/atlas (Creating Models for our posts)
-const CONNECTION_URL = 'mongodb+srv://YOUR_USER_NAME:Your_password@cluster0.4k9ccvn.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://learning:learning01@cluster0.4k9ccvn.mongodb.net/?retryWrites=true&w=majority';
 //use provisioned port or default to 5000
 const PORT = process.env.PORT || 5000;
 
@@ -25,4 +32,4 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
     .catch((error) => console.log(error.message));
 
 //to avoid console warnings
-mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);
