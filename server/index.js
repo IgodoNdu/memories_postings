2c927b5e52c021b0c1848ccc4a0d6d205775de78
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-14 10:40:53 +0100

    Basic backend settings
    Initialize express, bodyParser for our posts request, cors for cross origin request

--- server/index.js
+++ server/index.js
@@ -2,3 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+
+//as it's traditional practice with express, initialize the app
+const app = express();
+
+//setting up bodyParser to send our requests(post) as needed
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+//for cross origin request
+app.use(cors());
+
+//database: https://www.mongodb.com/cloud/atlas (Creating Models for our posts)
