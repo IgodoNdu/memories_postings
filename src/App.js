fb9968b969b10d749f9a69a2368b71f4e9073503
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 00:50:20 +0100

    Styling the frontend
    StyleSheet and stylings applied

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import memories from './images/memories.png';
 //import the needed components
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+//import styles
+import useStyles from './styles';
 
 
 function App() {
+  const classes = useStyles();
   return (
     //using material-ui
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
