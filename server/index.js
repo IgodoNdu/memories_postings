f1238209965cfdcf976ecaa013172bb623a1e39c
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-14 11:46:27 +0100

    Connection to MongoDB
    Connection to mongodb using the mongoose method

--- server/index.js
+++ server/index.js
@@ -13,3 +13,16 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 //database: https://www.mongodb.com/cloud/atlas (Creating Models for our posts)
+const CONNECTION_URL = 'mongodb+srv://YOUR_USER_NAME:Your_password@cluster0.4k9ccvn.mongodb.net/?retryWrites=true&w=majority';
+//use provisioned port or default to 5000
+const PORT = process.env.PORT || 5000;
+
+//Using mongoose to connect to our database with two parameters (returns a promise)
+//if connection is true, call the app with listen and it's params(port no, and call back func to run once the app listens successfully)
+//if connection isn't successful, return the error message
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)) )
+    .catch((error) => console.log(error.message));
+
+//to avoid console warnings
+mongoose.set('useFindAndModify', false);
