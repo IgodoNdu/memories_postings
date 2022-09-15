03f913a350014d1fb7ea03e9dda75835f3dfa45e
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 01:08:45 +0100

    Structure for API calls to backend
    Creating the structure for API calls to our backend

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+//here we make api calls to our backend
+import axios from "axios";
+
+//specify the url: for this project-case, url pointing to our backend route
+const url = 'http://localhost:5000/posts';
+
+export const fetchPosts = () => axios.get(url);
