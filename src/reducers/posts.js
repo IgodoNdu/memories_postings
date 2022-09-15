70195e43cd687667b47794ac935aa19bf67efc2f
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-16 00:00:32 +0100

    Proper matching syntax
    proper check value syntax (both type and value)

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,7 +3,7 @@ export default (posts = [], action) => {
     switch (action.type) {
         case 'UPDATE_POST':
             //if post._id == action,payload._id, return the updated post, else return the post as it was without any update
-            return posts.map((post) => post._id == action.payload._id ? action.payload : post);
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'FETCH_ALL_POST':
             return action.payload; //for now
         case 'CREATE_POST':
