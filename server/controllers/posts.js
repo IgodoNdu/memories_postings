ff4bd84db9d9195aa6b9041b142874830a6646d7
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 20:10:52 +0100

    Update a Post Controller
    Implementing the controller for post update route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,6 @@
 //Here we create the handlers for our routes for cleaner/efficient code. Extract the logic of the routes, and bring in here
 //Import the Model (access to the db model) required to do this (getPosts, createPosts)
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 //declare a function for the posts logic, and export it for use in the required router
@@ -31,4 +32,17 @@ export const createPost = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 
+}
+
+//function for post edit/update
+export const updatePost = async (req, res) => {
+    //get the id from the request, and rename the id to _id (a mongoose object id) via object destructuring
+    const { id: _id } = req.params;
+    const post = req.body;
+    //check if _id is a mongoose object id
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    //update the post if the _id is valid (from the model:PostMessage). Specify new=true, so we can receive the updated version of that post
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    //send the updated post
+    res.json(updatedPost);
 }
