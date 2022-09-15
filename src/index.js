3a908f4d34e2cb968485d731da1a26e065e8b7c6
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 01:45:47 +0100

    Initializing the redux store to the app
    Providing the redux store to the app, making the store/state available to all components of the frontend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+//Here we initialize redux to the app
+import { Provider } from 'react-redux';
+import { legacy_createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers'
+
+//setting up redux
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
