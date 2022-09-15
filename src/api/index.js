afc1b27c6ac3004dae2e72ea11c6e0aed35f2b55
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 21:37:37 +0100

    API call for updating a post
    Create the API call for updating a post

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,6 @@ const url = 'http://localhost:5000/posts';
 export const fetchPosts = () => axios.get(url);
 //api call for creating a post
 export const createPost = (newPost) => axios.post(url, newPost);
+//implement the api call for our update post route (passing the updated post, and post's ID as params)
+export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, updatePost);
+
