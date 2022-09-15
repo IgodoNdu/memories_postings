d3415dddf8e5cf2d36846f4ae83dd7aecd4267ff
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 03:13:04 +0100

    Data(posts) fetch from the global redux store
    Fetching the data(posts) from the global redux store/state, via the useSelector hook,  and confirming that it works with console.log

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 import Post from './Post/Post';
 //import styles
 import useStyles from './postStyles';
+//for fetching the data from the global redux store
+import { useSelector } from 'react-redux';
 
 const Posts = () => {
+  //for styling
   const classes = useStyles();
+  
+  //initializing useSelector as a hook with access to the global redux store/state, then return what you need i.e(state.property)
+  const posts = useSelector((state) => state.posts);
+  console.log(posts); // for now
+
   return (
     <div>
       <h1>Posts</h1>
