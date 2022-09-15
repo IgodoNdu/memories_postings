a8b26f564808c45e879227d064251b837a0bda19
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-15 20:09:14 +0100

    Post update via the post id
    Implementing the backend routing for updating a post via the post id

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,7 +2,7 @@
 //first import Express
 import express from "express";
 //import the controller(Logic) for the posts router
-import { getPosts, createPost } from "../controllers/posts.js";
+import { getPosts, createPost, updatePost } from "../controllers/posts.js";
 
 //Set up our router
 const router = express.Router();
@@ -10,6 +10,8 @@ const router = express.Router();
 //Now we start adding the routes (with it's logic controller): Routes and controllers separated for cleaner code base
 router.get('/', getPosts);
 router.post('/', createPost);
+//routing updates (patch i.e updating existing existing documents)
+router.patch('/:id', updatePost);
 
 //eXPORT THE WHOLE ROUTER
 export default router;
