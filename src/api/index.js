17a0b134554a058e7cb96456ce1b9b6ae4006d6e
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-16 01:27:49 +0100

    API Call for deleting a post
    Backend API for deleting a post

--- src/api/index.js
+++ src/api/index.js
@@ -9,4 +9,6 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 //implement the api call for our update post route (passing the updated post, and post's ID as params)
 export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, updatePost);
+//BackendAPI call for deleting a post
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
 
