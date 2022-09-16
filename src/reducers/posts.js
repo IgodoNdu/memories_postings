45bd75e03a4c634c9f6b6b45684c0855ebed51b2
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-16 01:45:54 +0100

    Update posts.js
    The reducer for deleting a post. Simply return all the posts excluding  a post whoose '_id' is same as the payload (_id in payload)

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,6 +1,9 @@
 //posts reducer: a function that takes the state, and with respect to the accompanying action, performs some logic
 export default (posts = [], action) => {
     switch (action.type) {
+        case 'DELETE_POST':
+            //return all the posts. filtering out the one we deleted
+            return posts.filter((post) => post._id !== action.payload);
         case 'UPDATE_POST':
             //if post._id == action,payload._id, return the updated post, else return the post as it was without any update
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
